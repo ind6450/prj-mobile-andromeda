5c6db66aad71720f13dd7986ef92b80a50b3baaa
ind6450 <sdhsdj6450@naver.com>
2017-10-28 17:46:50 +0900

    Android Login Test tutorial

---
+++ member/ind6450/Management/app/src/main/java/com/example/user/management/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.user.management;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.view.View.*;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+        Button loginButton = (Button) findViewById(R.id.loginBtn);
+
+        TextView registerButton = (TextView) findViewById(R.id.registerBtn);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+    }
+}
